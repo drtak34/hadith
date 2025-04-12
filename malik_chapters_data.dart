755cc53892f09fbb0f3999a539710e282f7ef78d
Seidi TAKOUK <drtak34@gmail.com>
2025-04-12 09:50:20 +0100

    Add files via upload

---
+++ malik_chapters_data.dart
@@ -0,0 +1,495 @@
+// Fichier généré automatiquement - Ne pas éditer manuellement
+// Généré le 2025-04-12 09:25:34
+
+class MalikChapters {
+  static const List<Map<String, dynamic>> chapters = [
+    {
+      'id': 1,
+      'englishTitle': "The Times of Prayer",
+      'arabicTitle': "كتاب 1",
+      'startHadith': 1,
+      'endHadith': 31,
+      'hadithCount': 31
+    },
+    {
+      'id': 2,
+      'englishTitle': "Purity",
+      'arabicTitle': "كتاب 2",
+      'startHadith': 32,
+      'endHadith': 145,
+      'hadithCount': 114
+    },
+    {
+      'id': 3,
+      'englishTitle': "Prayer",
+      'arabicTitle': "كتاب 3",
+      'startHadith': 146,
+      'endHadith': 221,
+      'hadithCount': 76
+    },
+    {
+      'id': 4,
+      'englishTitle': "Forgetfulness in Prayer",
+      'arabicTitle': "كتاب 4",
+      'startHadith': 222,
+      'endHadith': 224,
+      'hadithCount': 3
+    },
+    {
+      'id': 5,
+      'englishTitle': "Jumu\'a",
+      'arabicTitle': "كتاب 5",
+      'startHadith': 225,
+      'endHadith': 245,
+      'hadithCount': 21
+    },
+    {
+      'id': 6,
+      'englishTitle': "Prayer in Ramadan",
+      'arabicTitle': "كتاب 6",
+      'startHadith': 246,
+      'endHadith': 253,
+      'hadithCount': 8
+    },
+    {
+      'id': 7,
+      'englishTitle': "Tahajjud",
+      'arabicTitle': "كتاب 7",
+      'startHadith': 254,
+      'endHadith': 286,
+      'hadithCount': 33
+    },
+    {
+      'id': 8,
+      'englishTitle': "Prayer in Congregation",
+      'arabicTitle': "كتاب 8",
+      'startHadith': 287,
+      'endHadith': 325,
+      'hadithCount': 39
+    },
+    {
+      'id': 9,
+      'englishTitle': "Shortening the Prayer",
+      'arabicTitle': "كتاب 9",
+      'startHadith': 326,
+      'endHadith': 427,
+      'hadithCount': 102
+    },
+    {
+      'id': 10,
+      'englishTitle': "The Two \'Ids",
+      'arabicTitle': "كتاب 10",
+      'startHadith': 428,
+      'endHadith': 440,
+      'hadithCount': 13
+    },
+    {
+      'id': 11,
+      'englishTitle': "The Fear Prayer",
+      'arabicTitle': "كتاب 11",
+      'startHadith': 441,
+      'endHadith': 444,
+      'hadithCount': 4
+    },
+    {
+      'id': 12,
+      'englishTitle': "The Eclipse Prayer",
+      'arabicTitle': "كتاب 12",
+      'startHadith': 445,
+      'endHadith': 448,
+      'hadithCount': 4
+    },
+    {
+      'id': 13,
+      'englishTitle': "Asking for Rain",
+      'arabicTitle': "كتاب 13",
+      'startHadith': 449,
+      'endHadith': 454,
+      'hadithCount': 6
+    },
+    {
+      'id': 14,
+      'englishTitle': "The Qibla",
+      'arabicTitle': "كتاب 14",
+      'startHadith': 455,
+      'endHadith': 469,
+      'hadithCount': 15
+    },
+    {
+      'id': 15,
+      'englishTitle': "The Qur\'an",
+      'arabicTitle': "كتاب 15",
+      'startHadith': 470,
+      'endHadith': 519,
+      'hadithCount': 50
+    },
+    {
+      'id': 16,
+      'englishTitle': "Burials",
+      'arabicTitle': "كتاب 16",
+      'startHadith': 520,
+      'endHadith': 577,
+      'hadithCount': 58
+    },
+    {
+      'id': 17,
+      'englishTitle': "Zakat",
+      'arabicTitle': "كتاب 17",
+      'startHadith': 578,
+      'endHadith': 629,
+      'hadithCount': 52
+    },
+    {
+      'id': 18,
+      'englishTitle': "Fasting",
+      'arabicTitle': "كتاب 18",
+      'startHadith': 630,
+      'endHadith': 689,
+      'hadithCount': 60
+    },
+    {
+      'id': 19,
+      'englishTitle': "I\'tikaf in Ramadan",
+      'arabicTitle': "كتاب 19",
+      'startHadith': 690,
+      'endHadith': 704,
+      'hadithCount': 15
+    },
+    {
+      'id': 20,
+      'englishTitle': "Hajj",
+      'arabicTitle': "كتاب 20",
+      'startHadith': 705,
+      'endHadith': 957,
+      'hadithCount': 253
+    },
+    {
+      'id': 21,
+      'englishTitle': "Jihad",
+      'arabicTitle': "كتاب 21",
+      'startHadith': 959,
+      'endHadith': 1008,
+      'hadithCount': 50
+    },
+    {
+      'id': 22,
+      'englishTitle': "Vows and Oaths",
+      'arabicTitle': "كتاب 22",
+      'startHadith': 1009,
+      'endHadith': 1027,
+      'hadithCount': 19
+    },
+    {
+      'id': 23,
+      'englishTitle': "Sacrificial Animals",
+      'arabicTitle': "كتاب 23",
+      'startHadith': 1028,
+      'endHadith': 1041,
+      'hadithCount': 14
+    },
+    {
+      'id': 24,
+      'englishTitle': "Slaughtering Animals",
+      'arabicTitle': "كتاب 24",
+      'startHadith': 1042,
+      'endHadith': 1051,
+      'hadithCount': 10
+    },
+    {
+      'id': 25,
+      'englishTitle': "Game",
+      'arabicTitle': "كتاب 25",
+      'startHadith': 1052,
+      'endHadith': 1066,
+      'hadithCount': 15
+    },
+    {
+      'id': 26,
+      'englishTitle': "The \'Aqiqa",
+      'arabicTitle': "كتاب 26",
+      'startHadith': 1067,
+      'endHadith': 1073,
+      'hadithCount': 7
+    },
+    {
+      'id': 27,
+      'englishTitle': "Fara\'id",
+      'arabicTitle': "كتاب 27",
+      'startHadith': 1074,
+      'endHadith': 1089,
+      'hadithCount': 16
+    },
+    {
+      'id': 28,
+      'englishTitle': "Marriage",
+      'arabicTitle': "كتاب 28",
+      'startHadith': 1091,
+      'endHadith': 1149,
+      'hadithCount': 59
+    },
+    {
+      'id': 29,
+      'englishTitle': "Divorce",
+      'arabicTitle': "كتاب 29",
+      'startHadith': 1150,
+      'endHadith': 1271,
+      'hadithCount': 122
+    },
+    {
+      'id': 30,
+      'englishTitle': "Suckling",
+      'arabicTitle': "كتاب 30",
+      'startHadith': 1272,
+      'endHadith': 1289,
+      'hadithCount': 18
+    },
+    {
+      'id': 31,
+      'englishTitle': "Business Transactions",
+      'arabicTitle': "كتاب 31",
+      'startHadith': 1290,
+      'endHadith': 1385,
+      'hadithCount': 96
+    },
+    {
+      'id': 32,
+      'englishTitle': "Qirad",
+      'arabicTitle': "كتاب 32",
+      'startHadith': 1386,
+      'endHadith': 1387,
+      'hadithCount': 2
+    },
+    {
+      'id': 33,
+      'englishTitle': "Sharecropping",
+      'arabicTitle': "كتاب 33",
+      'startHadith': 1388,
+      'endHadith': 1389,
+      'hadithCount': 2
+    },
+    {
+      'id': 34,
+      'englishTitle': "Renting Land",
+      'arabicTitle': "كتاب 34",
+      'startHadith': 1390,
+      'endHadith': 1394,
+      'hadithCount': 5
+    },
+    {
+      'id': 35,
+      'englishTitle': "Pre-emption in Property",
+      'arabicTitle': "كتاب 35",
+      'startHadith': 1395,
+      'endHadith': 1398,
+      'hadithCount': 4
+    },
+    {
+      'id': 36,
+      'englishTitle': "Judgements",
+      'arabicTitle': "كتاب 36",
+      'startHadith': 1399,
+      'endHadith': 1454,
+      'hadithCount': 56
+    },
+    {
+      'id': 37,
+      'englishTitle': "Wills and Testaments",
+      'arabicTitle': "كتاب 37",
+      'startHadith': 1455,
+      'endHadith': 1463,
+      'hadithCount': 9
+    },
+    {
+      'id': 38,
+      'englishTitle': "Setting Free and Wala\'",
+      'arabicTitle': "كتاب 38",
+      'startHadith': 1464,
+      'endHadith': 1488,
+      'hadithCount': 25
+    },
+    {
+      'id': 39,
+      'englishTitle': "The Mukatab",
+      'arabicTitle': "كتاب 39",
+      'startHadith': 1489,
+      'endHadith': 1496,
+      'hadithCount': 8
+    },
+    {
+      'id': 40,
+      'englishTitle': "The Mudabbar",
+      'arabicTitle': "كتاب 40",
+      'startHadith': 1497,
+      'endHadith': 1499,
+      'hadithCount': 3
+    },
+    {
+      'id': 41,
+      'englishTitle': "Hudud",
+      'arabicTitle': "كتاب 41",
+      'startHadith': 1500,
+      'endHadith': 1536,
+      'hadithCount': 37
+    },
+    {
+      'id': 42,
+      'englishTitle': "Drinks",
+      'arabicTitle': "كتاب 42",
+      'startHadith': 1537,
+      'endHadith': 1551,
+      'hadithCount': 15
+    },
+    {
+      'id': 43,
+      'englishTitle': "Blood-Money",
+      'arabicTitle': "كتاب 43",
+      'startHadith': 1552,
+      'endHadith': 1595,
+      'hadithCount': 44
+    },
+    {
+      'id': 44,
+      'englishTitle': "The Oath of Qasama",
+      'arabicTitle': "كتاب 44",
+      'startHadith': 1596,
+      'endHadith': 1597,
+      'hadithCount': 2
+    },
+    {
+      'id': 45,
+      'englishTitle': "Madina",
+      'arabicTitle': "كتاب 45",
+      'startHadith': 1598,
+      'endHadith': 1622,
+      'hadithCount': 25
+    },
+    {
+      'id': 46,
+      'englishTitle': "The Decree",
+      'arabicTitle': "كتاب 46",
+      'startHadith': 1623,
+      'endHadith': 1632,
+      'hadithCount': 10
+    },
+    {
+      'id': 47,
+      'englishTitle': "Good Character",
+      'arabicTitle': "كتاب 47",
+      'startHadith': 1633,
+      'endHadith': 1650,
+      'hadithCount': 18
+    },
+    {
+      'id': 48,
+      'englishTitle': "Dress",
+      'arabicTitle': "كتاب 48",
+      'startHadith': 1651,
+      'endHadith': 1670,
+      'hadithCount': 20
+    },
+    {
+      'id': 49,
+      'englishTitle': "The Description of the Prophet, may Allah Bless Him and Grant Him Peace",
+      'arabicTitle': "كتاب 49",
+      'startHadith': 1671,
+      'endHadith': 1710,
+      'hadithCount': 40
+    },
+    {
+      'id': 50,
+      'englishTitle': "The Evil Eye",
+      'arabicTitle': "كتاب 50",
+      'startHadith': 1711,
+      'endHadith': 1729,
+      'hadithCount': 19
+    },
+    {
+      'id': 51,
+      'englishTitle': "Hair",
+      'arabicTitle': "كتاب 51",
+      'startHadith': 1730,
+      'endHadith': 1746,
+      'hadithCount': 17
+    },
+    {
+      'id': 52,
+      'englishTitle': "Visions",
+      'arabicTitle': "كتاب 52",
+      'startHadith': 1747,
+      'endHadith': 1755,
+      'hadithCount': 9
+    },
+    {
+      'id': 53,
+      'englishTitle': "Greetings",
+      'arabicTitle': "كتاب 53",
+      'startHadith': 1756,
+      'endHadith': 1762,
+      'hadithCount': 7
+    },
+    {
+      'id': 54,
+      'englishTitle': "General Subjects",
+      'arabicTitle': "كتاب 54",
+      'startHadith': 1763,
+      'endHadith': 1807,
+      'hadithCount': 45
+    },
+    {
+      'id': 55,
+      'englishTitle': "The Oath of Allegiance",
+      'arabicTitle': "كتاب 55",
+      'startHadith': 1808,
+      'endHadith': 1810,
+      'hadithCount': 3
+    },
+    {
+      'id': 56,
+      'englishTitle': "Speech",
+      'arabicTitle': "كتاب 56",
+      'startHadith': 1811,
+      'endHadith': 1838,
+      'hadithCount': 28
+    },
+    {
+      'id': 57,
+      'englishTitle': "Jahannam",
+      'arabicTitle': "كتاب 57",
+      'startHadith': 1839,
+      'endHadith': 1840,
+      'hadithCount': 2
+    },
+    {
+      'id': 58,
+      'englishTitle': "Sadaqa",
+      'arabicTitle': "كتاب 58",
+      'startHadith': 1841,
+      'endHadith': 1855,
+      'hadithCount': 15
+    },
+    {
+      'id': 59,
+      'englishTitle': "Knowledge",
+      'arabicTitle': "كتاب 59",
+      'startHadith': 1856,
+      'endHadith': 1856,
+      'hadithCount': 1
+    },
+    {
+      'id': 60,
+      'englishTitle': "The Supplication of the Unjustly Wronged",
+      'arabicTitle': "كتاب 60",
+      'startHadith': 1857,
+      'endHadith': 1857,
+      'hadithCount': 1
+    },
+    {
+      'id': 61,
+      'englishTitle': "The Names of the Prophet, may Allah Bless Him and Grant Him Peace",
+      'arabicTitle': "كتاب 61",
+      'startHadith': 1858,
+      'endHadith': 1858,
+      'hadithCount': 1
+    },
+  ];
+}
